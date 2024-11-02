fe8457d32de29b02d4701305d8450f5891b1c8a8
J3anSimas <jeansimas16@gmail.com>
2024-11-02 20:21:36 -0300

    Fazendo pequenos ajustes

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Teste da desgraça")
+		return c.String(http.StatusOK, "Agora vai pdv")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
